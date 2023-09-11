1dacebe2cd6d15590fc731770412e71f81c6bc95
MahendraPachipala <141004162+MahendraPachipala@users.noreply.github.com>
2023-09-11 17:30:09 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,38 @@
+const textbtn = document.getElementById("btn");
+textbtn.addEventListener("click", function(){
+    const place = document.getElementById("TaskContainer");
+   
+    const inputbar = document.createElement("input");
+    inputbar.type = "text";
+    inputbar.setAttribute("id", "inputbar");
+    inputbar.placeholder="Enter your Task..."
+    
+    place.appendChild(inputbar);
+    
+    const Addbutton = document.createElement("button");
+    Addbutton.textContent = "Add Task";
+    Addbutton.setAttribute("id", "AddButton");
+    place.appendChild(Addbutton);
+    document.getElementById("TaskContainer").style.display = "block";
+    document.getElementById("btn").style.display = "none";
+    //function
+    Addbutton.addEventListener("click", function(){
+        const Taskholder = document.createElement("div");
+        document.getElementById("Taskdiv").append(Taskholder);
+        const labelTask = document.createElement("label");
+        labelTask.htmlFor = "task1";
+        labelTask.textContent = inputbar.value; 
+        const inputTask = document.createElement("input");
+        inputTask.type = "checkbox";
+        inputTask.id = "task1";
+        
+        Taskholder.appendChild(inputTask);
+        Taskholder.appendChild(labelTask);
+        inputbar.value = "";
+        document.getElementById("inputbar").remove();
+        document.getElementById("AddButton").remove();
+        document.getElementById("btn").style.display = "block";
+        
+
+    });
+});
