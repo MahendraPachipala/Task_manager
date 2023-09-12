6ade7b92f7207f99a71c0ecb17dde9c256e474ae
MahendraPachipala <141004162+MahendraPachipala@users.noreply.github.com>
2023-09-12 14:55:34 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,39 +1,45 @@
 const textbtn = document.getElementById("btn");
+let i = 1;
+
 textbtn.addEventListener("click", function(){
     const place = document.getElementById("TaskContainer");
-   
+
     const inputbar = document.createElement("input");
     inputbar.type = "text";
     inputbar.setAttribute("id", "inputbar");
-    inputbar.placeholder="Enter your Task..."
-    
+    inputbar.placeholder = "Enter your Task..."
+
     place.appendChild(inputbar);
-    
+
     const Addbutton = document.createElement("button");
     Addbutton.textContent = "Add Task";
     Addbutton.setAttribute("id", "AddButton");
     place.appendChild(Addbutton);
-    document.getElementById("TaskContainer").style.display = "block";
     document.getElementById("btn").style.display = "none";
     document.getElementById("dummy").style.display = "none";
-    
+
     Addbutton.addEventListener("click", function(){
         const Taskholder = document.createElement("div");
         document.getElementById("Taskdiv").append(Taskholder);
         const labelTask = document.createElement("label");
-        labelTask.htmlFor = "task1";
+        let tasknum = "task" + i;
+        labelTask.htmlFor = tasknum;
+        labelTask.setAttribute("id", tasknum);
         labelTask.textContent = inputbar.value; 
         const inputTask = document.createElement("input");
         inputTask.type = "checkbox";
-        inputTask.id = "task1";
-        
+        inputTask.setAttribute("id", tasknum);
+        inputTask.setAttribute("name", tasknum);
+
         Taskholder.appendChild(inputTask);
         Taskholder.appendChild(labelTask);
         inputbar.value = "";
         document.getElementById("inputbar").remove();
         document.getElementById("AddButton").remove();
-        document.getElementById("btn").style.display = "block";
-        
-
+        document.getElementById("btn").style.display = "inline";
+        i++;
+    
     });
 });
+
+
